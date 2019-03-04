9640bfe478b5650da6f2c1368a0f9cb26249cd3f
mumu <longtian.jiamu@gmail.com>
2019-03-04 15:44:59 +0800

    no message

--- 2019022702/mumu414/calculator.py
+++ 2019022702/mumu414/calculator.py
@@ -22,7 +22,7 @@ def divide(x,y):
 # 用户输入
 print("选择运算：")
 print("1、相加")
-print("2、相乘")
+print("2、相减")
 print("3、相乘")
 print("4、相除")
 
@@ -34,13 +34,13 @@ num2 = int(input("输入第二个数字："))
 if choice == '1':
     print(num1,"+",num2,"=",add(num1,num2))
 
-if choice == '2':
+elif choice == '2':
     print(num1,"-",num2,"=",subtract(num1,num2))
 
-if choice == '3':
+elif choice == '3':
     print(num1,"*",num2,"=",multiply(num1,num2))
 
-if choice == '4':
+elif choice == '4':
     print(num1,"/",num2,"=",divide(num1,num2))
 
 else:
